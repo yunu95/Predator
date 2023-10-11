93490a2bb386ba03e1ad3e9a802fef8b9edead9e
KSC <oeodo0116@naver.com>
2023-10-11 10:40:19 +0900

    [PRED-81] Texture 설정 가능 버전 커밋

--- Yunu3DGraphicsInterface/IMaterial.h
+++ Yunu3DGraphicsInterface/IMaterial.h
@@ -9,6 +9,15 @@
 
 namespace yunuGI
 {
+	enum class Texture_Type
+	{
+		ALBEDO,
+		NORMAL,
+		ARM,
+		HEIGHT,
+		EMISSION
+	};
+
 	struct MaterialDesc
 	{
 		std::wstring materialName;
@@ -20,7 +29,7 @@ namespace yunuGI
 		virtual void SetMaterialName(const std::wstring& materialName) = 0;
 		virtual void SetVertexShader(const std::wstring& vertexShader) = 0;
 		virtual void SetPixelShader(const std::wstring& pixelShader) = 0;
-		virtual void SetTexture(const int index, const std::wstring& texture) = 0;
+		virtual void SetTexture(Texture_Type textureType, const std::wstring& texture) = 0;
 		virtual void SetColor(const yunuGI::Color& color) = 0;
 
 		virtual const std::wstring& GetMaterialName()const = 0;
