05a48153b8c639380f9b301c425b7d1b8a713869
yunu95 <liunux95@gmail.com>
2023-12-18 12:21:12 +0900

    테스트 배치파일이 배치파일이 위치한 경로에 의존해 명령을 실행하도록 변경

--- RunTests.bat
+++ RunTests.bat
@@ -1,9 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 set lineNum=0
-for /f "tokens=*" %%i in ('vstest.console.exe --listtests .\Bin\x64\Debug\InWanderLand.dll') do (
+for /f "tokens=*" %%i in ('vstest.console.exe --listtests %~dp0\Bin\x64\Debug\InWanderLand.dll') do (
 	set /a lineNum+=1
     if !lineNum! geq 4 (
-	vstest.console.exe --tests:%%i .\Bin\x64\Debug\InWanderLand.dll
+	vstest.console.exe --tests:%%i %~dp0\Bin\x64\Debug\InWanderLand.dll
     )
 )
