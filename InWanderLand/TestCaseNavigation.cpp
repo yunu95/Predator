61066dbfd386b5c2e7ad5d45e24e8e24c071a175
yunu95 <liunux95@gmail.com>
2023-12-19 11:23:43 +0900

    테스트 코드들이 모두 동작가능하게 변경

--- InWanderLand/TestCaseNavigation.cpp
+++ InWanderLand/TestCaseNavigation.cpp
@@ -60,7 +60,7 @@ void TestCaseNavigationInit()
 
     auto camObj = yunutyEngine::Scene::getCurrentScene()->AddGameObject();
     auto rtsCam = camObj->AddComponent<RTSCam>();
-    rtsCam->GetTransform()->position = Vector3d(0, 10, 0);
+    rtsCam->GetTransform()->position = Vector3d(3, 10, 3);
 
     CreateNavPlane({ -2,0,-8 }, { 2,0,8 }, worldVertices, worldFaces);
     CreateNavPlane({ -8,0,-2 }, { 8,0,2 }, worldVertices, worldFaces);
@@ -80,21 +80,24 @@ void TestCaseNavigationInit()
     {
         agent->MoveTo(position);
     };
-    agent->MoveTo(Vector3d{ 7,0,7 });
+    const Vector3d moveDestination{ 6.5,0,6.5 };
+    agent->MoveTo(moveDestination);
+    agent->SetSpeed(30);
+    agent->SetAcceleration(10000000);
     // delayedTestFunctions에 2초 후 실행시킬 콜백 함수를 등록합니다. 이 콜백함수는 게임 엔진 스레드에서 호출됩니다.
-    delayedTestFunctions->todoList.push_back({ 8,[=]() {
+    delayedTestFunctions->todoList.push_back({ 1,[=]() {
         // 게임 엔진 스레드에서 메인 스레드에서 특정 동작을 구동시키고 싶다면 아래의 AddMainLoopTodo 함수를 사용합니다.
         Application::Application::GetInstance().AddMainLoopTodo([=]() {
             // Assert 함수군은 테스트 케이스의 실행 성공 여부를 판단하는데에 쓰입니다.
             // Assert의 실행은 메인 스레드에서 실행되어야 합니다.
-            Assert::IsTrue((agent->GetTransform()->GetWorldPosition() - Vector3d{7,0,7}).MagnitudeSqr() < 0.3,L"네비게이션 에이전트가 이동을 수행하지 못했습니다.");
+            Assert::IsTrue((agent->GetTransform()->GetWorldPosition() - moveDestination).MagnitudeSqr() < 0.3,L"navigation agent couldn't move to a specific location!");
             agent->Relocate(Vector3d{ -10,0,0 });
                 });
             } });
-    delayedTestFunctions->todoList.push_back({ 10,[=]() {
+    delayedTestFunctions->todoList.push_back({ 1.2,[=]() {
         // 게임 엔진 스레드에서 메인 스레드에서 특정 동작을 구동시키고 싶다면 아래의 AddMainLoopTodo 함수를 사용합니다.
         Application::Application::GetInstance().AddMainLoopTodo([=]() {
-            Assert::IsTrue((agent->GetTransform()->GetWorldPosition() - Vector3d{-7.5,0,0}).MagnitudeSqr() < 1,L"네비게이션 에이전트의 Relocate 함수가 순간이동을 수행하지 못했습니다.");
+            Assert::IsTrue((agent->GetTransform()->GetWorldPosition() - Vector3d{-7.5,0,0}).MagnitudeSqr() < 1,L"navigation agent's ""Relocate"" method didn't really relocate the agent!");
             // 위 식이 참이라면 프로그램을 종료합니다. 
             Application::Application::GetInstance().TurnOff();
                 });
@@ -109,7 +112,6 @@ namespace InWanderLand
     public:
         TEST_METHOD(TestCaseNavigation)
         {
-            Assert::IsTrue(1 + 1 == 3);
             Application::Application& client = Application::Application::CreateApplication(0, 0);
             Application::Contents::ContentsLayer::AssignTestInitializer(TestCaseNavigationInit);
             client.Initialize();
