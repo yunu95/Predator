966b940a573ddc317aea0901a2f57f18d672a8a7
KSC <oeodo0116@naver.com>
2023-12-22 10:50:43 +0900

    [PRED-157] test코드에 주석 추가 및 카메라 위치 변경

--- InWanderLand/SnippetAnimationChangeInterpolation.cpp
+++ InWanderLand/SnippetAnimationChangeInterpolation.cpp
@@ -21,8 +21,8 @@ void SnippetAnimationChangeInterpolationInit()
 	}
 
 	auto camObj = yunutyEngine::Scene::getCurrentScene()->AddGameObject();
-
-	auto rtsCam = camObj->AddComponent<RTSCam>();
+	camObj->AddComponent<yunutyEngine::graphics::Camera>();
+	camObj->GetTransform()->position = Vector3d{ 0,4,-15 };
 
 	const yunuGI::IResourceManager* _resourceManager = yunutyEngine::graphics::Renderer::SingleInstance().GetResourceManager();
 
@@ -35,6 +35,7 @@ void SnippetAnimationChangeInterpolationInit()
 			float temp = 2 * i;
 			float temp2 = 2 * j;
 			auto object = yunutyEngine::Scene::getCurrentScene()->AddGameObjectFromFBX("Boss");
+			
 			auto test = object->AddComponent<TestComponent2>();
 			object->GetTransform()->position = Vector3d{ temp,temp2,0 };
 			auto animator = object->GetComponent<yunutyEngine::graphics::Animator>();
@@ -67,12 +68,11 @@ void SnippetAnimationChangeInterpolationInit()
 
 namespace snippets
 {
+	
 	TEST_CLASS(InWanderLand)
 	{
 	public:
-		// 테스트 함수의 이름이 Snippet으로 시작하는 테스트들은 빌드의 성공 여부 판단에 쓰이지 않습니다.
-		// RunTests.bat를 실행해도 이 테스트들은 실행되지 않으며, Jenkins에서도 이 테스트들은 실행되지 않습니다.
-		// 이 테스트들은 오직 개발자가 직접 엔트리 포인트를 달리하여 테스트를 진행하고 싶을 때의 용도로만 사용됩니다.
+		// TestComponent2안에 H키와 J키를 눌렀을 때 애니메이션 전환하게 되어있음.
 		TEST_METHOD(SnippetAnimationChangeInterpolation)
 		{
 			application::Application& client = application::Application::CreateApplication(0, 0);
