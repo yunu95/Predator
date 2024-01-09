32d8234ffa7565e1a1b8f55f3a86e30e44ab8288
KSJ <junjunpp03@gmail.com>
2024-01-09 19:19:28 +0900

    [PRED-87] Event System 추가

--- InWanderLand/Application.h
+++ InWanderLand/Application.h
@@ -6,10 +6,12 @@
 
 #include "Layer.h"
 #include "CommandManager.h"
+#include "EventManager.h"
 
 #include <vector>
 #include <memory>
 #include <functional>
+#include <type_traits>
 #include <mutex>
 
 namespace application
@@ -43,15 +45,46 @@ namespace application
 
         const ApplicationSpecification& GetApplicationSpecification() const;
         
+        // 게임 엔진의 그래픽스 엔진으로부터 최종 렌더 결과를 받아내는 함수
         void* GetSceneSRV();
 
+#ifdef EDITOR
+        // 이벤트를 생성하여 처리하는 함수
+        template<typename Event, bool dispatchImmediately = false, typename... EventArgs>
+        static void DispatchEvent(EventArgs&&... args)
+        {
+            static_assert(std::is_base_of_v<editor::EditorEvents, Event>, "You can use this function only for Events that inherit \"EditorEvents\".");
+
+            static auto& app = GetInstance();
+
+            std::shared_ptr<Event> event = std::make_shared<Event>(std::forward<EventArgs>(args)...);
+            if constexpr (dispatchImmediately)
+            {
+                app.OnEvent(*event);
+            }
+            else
+            {
+                app.em.PushEventCallable([event]() { app.OnEvent(*event); });
+            }
+        }
+#endif
+
     private:
+#ifdef EDITOR
+        // ImGui 단계 처리
         void ImGuiUpdate();
+        // 전달 받은 이벤트에 대한 처리
+        void OnEvent(editor::EditorEvents& event);
+        // 이벤트들을 일괄적으로 실행
+        void ProcessEvents();
+#endif
         std::mutex loopTodoRegistrationMutex;
         // AddMainLoopTodo로 등록된 휘발성 콜백 함수들입니다.
         // 매 루프가 종료될 때 이 컨테이너에 실행 동작들이 담겨있다면 모두 실행하고 내용을 비웁니다.
         // 이 목록에 담긴 함수들이 실행되는 동안 게임 엔진 스레드는 동작을 정지합니다.
         std::vector<std::function<void()>> loopRegistrations;
+
+        void SetEditorResizeCallBack(std::function<void()> callBack);
 
         enum class LayerList
         {
@@ -71,6 +104,7 @@ namespace application
 
 #ifdef EDITOR
         editor::CommandManager& cm = editor::CommandManager::GetSingletonInstance();
+        editor::EventManager em = editor::EventManager();
 #endif
     };
 }
