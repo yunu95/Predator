28d7d7079cf22aef9e12d4ab0865064c2e999434
yunu95 <liunux95@gmail.com>
2024-01-10 21:20:05 +0900

    컬러들이 red,blue,green등 기본 컬러들이 알파값을 1로 가지게 만듬

--- Yunu3DGraphicsInterface/YunuGIColor.h
+++ Yunu3DGraphicsInterface/YunuGIColor.h
@@ -13,10 +13,10 @@ namespace yunuGI
         bool operator==(const Color& other) const {
             return r == other.r && g == other.g && b == other.b && a == other.a;
         }
-        static Color red() { return { 1,0,0,0 }; };
-        static Color blue() { return { 0,0,1,0 }; };
-        static Color green() { return { 0,1,0,0 }; };
-        static Color white() { return { 1,1,1,0 }; };
-        static Color black() { return { 0,0,0,0 }; };
+        static Color red() { return { 1,0,0,1 }; };
+        static Color blue() { return { 0,0,1,1 }; };
+        static Color green() { return { 0,1,0,1 }; };
+        static Color white() { return { 1,1,1,1 }; };
+        static Color black() { return { 0,0,0,1 }; };
     };
 }
