63edcbc48bf7668b980e5e12ce6c1d0287228958
yunu95 <liunux95@gmail.com>
2024-01-16 19:05:09 +0900

    테스트 배치파일 변경

--- RunTests.bat
+++ RunTests.bat
@@ -6,20 +6,12 @@ set errorFlag=0
 IF EXIST %~dp0\testResults.log (
     del %~dp0\testResults.log
 )
-for /f "tokens=*" %%i in ('vstest.console.exe --listtests %~dp0\Bin\x64\Debug\InWanderLand.dll') do (
-	set /a lineNum+=1
-	if !lineNum! geq 4 (
-		vstest.console.exe --testcasefilter:"Name=%%i&Name^!~Snippet" %~dp0\Bin\x64\Debug\InWanderLand.dll -- RunConfiguration.TestSessionTimeout=20000 >> %~dp0\testResults.log
-		if errorlevel 1 (
-			set errorFlag=1
-		)
-    	)
-)
 for /f "tokens=*" %%i in ('vstest.console.exe --listtests %~dp0\Bin\x64\Release\InWanderLand.dll') do (
 	set /a lineNum+=1
 	if !lineNum! geq 4 (
 		vstest.console.exe --testcasefilter:"Name=%%i&Name^!~Snippet" %~dp0\Bin\x64\Release\InWanderLand.dll -- RunConfiguration.TestSessionTimeout=20000 >> %~dp0\testResults.log
 		if errorlevel 1 (
+			echo something has gone wrong!
 			set errorFlag=1
 		)
     	)
