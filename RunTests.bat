9e63a4714e85ef98d6cfea4a1decfab08a8a5dde
yunu95 <liunux95@gmail.com>
2024-01-16 19:08:05 +0900

    릴리즈 / 디버그 테스트를 모두 온전히 진행하도록 새로 배치파일 커밋

--- RunTests.bat
+++ RunTests.bat
@@ -6,6 +6,17 @@ set errorFlag=0
 IF EXIST %~dp0\testResults.log (
     del %~dp0\testResults.log
 )
+for /f "tokens=*" %%i in ('vstest.console.exe --listtests %~dp0\Bin\x64\Debug\InWanderLand.dll') do (
+	set /a lineNum+=1
+	if !lineNum! geq 4 (
+		vstest.console.exe --testcasefilter:"Name=%%i&Name^!~Snippet" %~dp0\Bin\x64\Debug\InWanderLand.dll -- RunConfiguration.TestSessionTimeout=20000 >> %~dp0\testResults.log
+		if errorlevel 1 (
+			echo something has gone wrong!
+			set errorFlag=1
+		)
+    	)
+)
+set lineNum=0
 for /f "tokens=*" %%i in ('vstest.console.exe --listtests %~dp0\Bin\x64\Release\InWanderLand.dll') do (
 	set /a lineNum+=1
 	if !lineNum! geq 4 (
