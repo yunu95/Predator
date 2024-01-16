180721f10e61e7313379daedb87b4c0b245814a2
yunu95 <liunux95@gmail.com>
2024-01-16 18:28:09 +0900

    빌드테스트가 디버그 빌드와 릴리즈 빌드를 둘 다 테스트하도록 테스트용 배치 파일을 변경

--- RunTests.bat
+++ RunTests.bat
@@ -15,6 +15,15 @@ for /f "tokens=*" %%i in ('vstest.console.exe --listtests %~dp0\Bin\x64\Debug\In
 		)
     	)
 )
+for /f "tokens=*" %%i in ('vstest.console.exe --listtests %~dp0\Bin\x64\Release\InWanderLand.dll') do (
+	set /a lineNum+=1
+	if !lineNum! geq 4 (
+		vstest.console.exe --testcasefilter:"Name=%%i&Name^!~Snippet" %~dp0\Bin\x64\Release\InWanderLand.dll -- RunConfiguration.TestSessionTimeout=20000 >> %~dp0\testResults.log
+		if errorlevel 1 (
+			set errorFlag=1
+		)
+    	)
+)
 type %~dp0\testResults.log
 
 if %errorFlag% equ 1 (
