ad35c777cf5a7f45abab80443c79e4158d0ba737
yunu95 <liunux95@gmail.com>
2024-01-18 11:28:53 +0900

    게임엔진의 Input 기능을 끄고 킬 수 있도록 코드를 변경

--- Yunuty/header/Input.h
+++ Yunuty/header/Input.h
@@ -30,9 +30,15 @@ namespace yunutyEngine
         public GlobalComponent
     {
     public:
+        // 사용자가 키를 누르고 있는가?
         static bool isKeyDown(KeyCode keyCode);
+        // 해당 프레임에 키가 갓 눌렸는가?
         static bool isKeyPushed(KeyCode keyCode);
+        // 해당 프레임에 키가 갓 떼어졌는가?
         static bool isKeyLifted(KeyCode keyCode);
+        // 인풋 시스템을 활성화 / 비활성화합니다. 게임 윈도우 창이 포커스되거나 포커스를 잃었을 때 알맞게 호출하십시오.
+        static void SetActive(bool active);
+        static bool IsActive();
         static Vector2d getMouseScreenPosition();
         static Vector2d getMouseScreenPositionNormalized();
         static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
@@ -45,12 +51,15 @@ namespace yunutyEngine
         unordered_set<KeyCode> keyPushedSet;
         unordered_set<KeyCode> keyLiftedSet;
         unordered_map<unsigned int, KeyCode> rawKeyEnumMap;
+        Vector2d cachedMouseScreenPosition;
         Input();
         bool m_isKeyDown(KeyCode keyCode);
         bool m_isKeyPushed(KeyCode keyCode);
         bool m_isKeyLifted(KeyCode keyCode);
+        void CacheMouseScreenPosition();
         virtual ~Input();
         HWND mainWnd{ 0 };
+        bool isActive{ true };
 
         friend graphics::Renderer;
     };
