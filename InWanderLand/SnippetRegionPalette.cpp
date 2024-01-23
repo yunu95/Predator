9f21c5e3f737660acd98c888e449709fdc5f4a97
yunu95 <liunux95@gmail.com>
2024-01-23 17:07:27 +0900

    스니펫 코드들 중 팔레트 기능 시연에 해당하는 코드들을 모두 주석처리함 (사유 : 오래됨)

--- InWanderLand/SnippetRegionPalette.cpp
+++ InWanderLand/SnippetRegionPalette.cpp
@@ -7,8 +7,8 @@
 #include "Application.h"
 #include "RTSCam.h"
 #include "SelectionBox.h"
-#include "TerrainPaletteManager.h"
-#include "RegionPaletteManager.h"
+#include "TerrainPalette.h"
+#include "RegionPalette.h"
 #include "RegionInstance.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -16,53 +16,53 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace application::editor::palette;
 
 // 이 함수는 게임의 기본 초기화 함수를 오버라이드합니다.
-void SnippetInitializerRegionPalette()
-{
-    yunutyEngine::Scene::LoadScene(new yunutyEngine::Scene());
-    auto rtsCam = Scene::getCurrentScene()->AddGameObject()->AddComponent<RTSCam>();
-
-    rtsCam->GetTransform()->position = Vector3d(3, 10, 3);
-    application::editor::palette::Palette::SetCurrentPalette(&application::editor::palette::RegionPalette::SingleInstance());
-    rtsCam->groundHoveringClickCallback = [](const Vector3d& worldPos) {application::editor::palette::Palette::GetCurrentPalette()->OnMouseMove(worldPos); };
-    rtsCam->groundLeftClickCallback = [](const Vector3d& worldPos) {application::editor::palette::Palette::GetCurrentPalette()->OnLeftClick(); };
-    rtsCam->groundLeftClickReleaseCallback = [](const Vector3d& worldPos) {application::editor::palette::Palette::GetCurrentPalette()->OnLeftClickRelease(); };
-    rtsCam->deleteButtonCallback = []() {application::editor::palette::Palette::GetCurrentPalette()->OnDeletion(); };
-    rtsCam->xButtonCallback = []()
-    {
-        auto palette = application::editor::palette::Palette::GetCurrentPalette();
-        palette->SetAsSelectMode(!palette->IsSelectMode());
-    };
-
-    auto directionalLight = Scene::getCurrentScene()->AddGameObject()->AddComponent<yunutyEngine::graphics::DirectionalLight>();
-
-    auto tempRegion1 = Scene::getCurrentScene()->AddGameObject()->AddComponent<RegionInstance>();
-    tempRegion1->GetTransform()->position = Vector3d(0, 0, 0);
-    auto tempRegion2 = Scene::getCurrentScene()->AddGameObject()->AddComponent<RegionInstance>();
-    tempRegion2->GetTransform()->position = Vector3d(3, 0, 0);
-    auto tempRegion3 = Scene::getCurrentScene()->AddGameObject()->AddComponent<RegionInstance>();
-    tempRegion3->GetTransform()->position = Vector3d(0, 0, 3);
-    tempRegion3->SetHalfExtent({ 1.5,2 });
-
-    SelectionBox::Instance();
-    yunutyEngine::YunutyCycle::SingleInstance().Play();
-}
-
-namespace snippets
-{
-    TEST_CLASS(SnippetRegionPaletteClass)
-    {
-    public:
-        // 테스트 함수의 이름이 Snippet으로 시작하는 테스트들은 빌드의 성공 여부 판단에 쓰이지 않습니다.
-        // RunTests.bat를 실행해도 이 테스트들은 실행되지 않으며, Jenkins에서도 이 테스트들은 실행되지 않습니다.
-        // 이 테스트들은 오직 개발자가 직접 엔트리 포인트를 달리하여 테스트를 진행하고 싶을 때의 용도로만 사용됩니다.
-        TEST_METHOD(SnippetRegionPalette)
-        {
-            application::Application& client = application::Application::CreateApplication(0, 0);
-            application::contents::ContentsLayer::AssignTestInitializer(SnippetInitializerRegionPalette);
-            client.Initialize();
-            client.Run();
-            client.Finalize();
-        }
-    };
-}
+//void SnippetInitializerRegionPalette()
+//{
+//    yunutyEngine::Scene::LoadScene(new yunutyEngine::Scene());
+//    auto rtsCam = Scene::getCurrentScene()->AddGameObject()->AddComponent<RTSCam>();
+//
+//    rtsCam->GetTransform()->position = Vector3d(3, 10, 3);
+//    application::editor::palette::Palette::SetCurrentPalette(&application::editor::palette::RegionPalette::SingleInstance());
+//    rtsCam->groundHoveringClickCallback = [](const Vector3d& worldPos) {application::editor::palette::Palette::GetCurrentPalette()->OnMouseMove(worldPos); };
+//    rtsCam->groundLeftClickCallback = [](const Vector3d& worldPos) {application::editor::palette::Palette::GetCurrentPalette()->OnLeftClick(); };
+//    rtsCam->groundLeftClickReleaseCallback = [](const Vector3d& worldPos) {application::editor::palette::Palette::GetCurrentPalette()->OnLeftClickRelease(); };
+//    rtsCam->deleteButtonCallback = []() {application::editor::palette::Palette::GetCurrentPalette()->OnDeletion(); };
+//    rtsCam->xButtonCallback = []()
+//    {
+//        auto palette = application::editor::palette::Palette::GetCurrentPalette();
+//        palette->SetAsSelectMode(!palette->IsSelectMode());
+//    };
+//
+//    auto directionalLight = Scene::getCurrentScene()->AddGameObject()->AddComponent<yunutyEngine::graphics::DirectionalLight>();
+//
+//    auto tempRegion1 = Scene::getCurrentScene()->AddGameObject()->AddComponent<RegionInstance>();
+//    tempRegion1->GetTransform()->position = Vector3d(0, 0, 0);
+//    auto tempRegion2 = Scene::getCurrentScene()->AddGameObject()->AddComponent<RegionInstance>();
+//    tempRegion2->GetTransform()->position = Vector3d(3, 0, 0);
+//    auto tempRegion3 = Scene::getCurrentScene()->AddGameObject()->AddComponent<RegionInstance>();
+//    tempRegion3->GetTransform()->position = Vector3d(0, 0, 3);
+//    tempRegion3->SetHalfExtent({ 1.5,2 });
+//
+//    SelectionBox::Instance();
+//    yunutyEngine::YunutyCycle::SingleInstance().Play();
+//}
+//
+//namespace snippets
+//{
+//    TEST_CLASS(SnippetRegionPaletteClass)
+//    {
+//    public:
+//        // 테스트 함수의 이름이 Snippet으로 시작하는 테스트들은 빌드의 성공 여부 판단에 쓰이지 않습니다.
+//        // RunTests.bat를 실행해도 이 테스트들은 실행되지 않으며, Jenkins에서도 이 테스트들은 실행되지 않습니다.
+//        // 이 테스트들은 오직 개발자가 직접 엔트리 포인트를 달리하여 테스트를 진행하고 싶을 때의 용도로만 사용됩니다.
+//        TEST_METHOD(SnippetRegionPalette)
+//        {
+//            application::Application& client = application::Application::CreateApplication(0, 0);
+//            application::contents::ContentsLayer::AssignTestInitializer(SnippetInitializerRegionPalette);
+//            client.Initialize();
+//            client.Run();
+//            client.Finalize();
+//        }
+//    };
+//}
 #endif
