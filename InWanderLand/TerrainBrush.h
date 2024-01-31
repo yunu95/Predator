90fdaa82253cf1c03376e898609eabbede73d0ae
yunu95 <liunux95@gmail.com>
2024-01-31 15:33:45 +0900

    팔레트 브러시가 정상적으로 켜졌다 꺼지도록 변경(컴포넌트의 OnEnable, OnDisable을 호출하는 로직을 개선함) 지형 팔레트가 정상적으로 지형을 네비게이션 메시로 구울 수 있도록 변경

--- InWanderLand/TerrainBrush.h
+++ InWanderLand/TerrainBrush.h
@@ -6,97 +6,89 @@
 
 namespace application
 {
-	namespace editor
-	{
-		namespace palette
-		{
-			class TerrainBrush : public yunutyEngine::Component, public yunutyEngine::SingletonComponent<TerrainBrush>
-			{
-			public:
-				void SetBrushSize(unsigned int size)
-				{
-					auto expectedNum = (size * 2 + 1) * (size * 2 + 1);
+    namespace editor
+    {
+        namespace palette
+        {
+            class TerrainBrush : public yunutyEngine::Component, public yunutyEngine::SingletonComponent<TerrainBrush>
+            {
+            public:
+                void SetBrushSize(unsigned int size)
+                {
+                    brushSize = size;
+                    auto expectedNum = (size * 2 + 1) * (size * 2 + 1);
 
-					for (int i = planeMeshes.size(); i < expectedNum; i++)
-					{
-						auto planeMesh = AttachDebugMesh(GetGameObject()->AddGameObject(), DebugMeshType::Rectangle, yunuGI::Color::green(), true);
-						planeMesh->GetTransform()->SetLocalScale( Vector3d::one * TerrainPalette::nodeDistance);
-						planeMeshes.push_back(planeMesh);
-						/*
-						25 26 27 28 29 30 31
-						48 09 10 11 12 13 32
-						47 24 01 02 03 14 33
-						46 23 08 00 04 15 34
-						45 22 07 06 05 16 35
-						44 21 20 19 18 17 36
-						43 42 41 40 39 38 37
-						*/
-						// 레이어 번호 1    2    3    4
-						// 루트 값    1,2  3,4  5,6  7,8
-						int layerIdx = sqrt(i) / 2.0 + 0.5;
+                    for (int i = planeMeshes.size(); i < expectedNum; i++)
+                    {
+                        auto planeMesh = AttachDebugMesh(GetGameObject()->AddGameObject(), DebugMeshType::Rectangle, yunuGI::Color::green(), true);
+                        planeMesh->GetTransform()->SetLocalScale(Vector3d::one * TerrainPalette::nodeDistance);
+                        planeMeshes.push_back(planeMesh);
+                        /*
+                        25 26 27 28 29 30 31
+                        48 09 10 11 12 13 32
+                        47 24 01 02 03 14 33
+                        46 23 08 00 04 15 34
+                        45 22 07 06 05 16 35
+                        44 21 20 19 18 17 36
+                        43 42 41 40 39 38 37
+                        */
+                        // 레이어 번호 1    2    3    4
+                        // 루트 값    1,2  3,4  5,6  7,8
+                        int layerIdx = sqrt(i) / 2.0 + 0.5;
 
-						// 우방 기수번호 = (레이어 *2+1)^2
-						// 하방 기수번호 = (레이어 *2+1)^2 + 레이어 * 2 * 1
-						// 좌방 기수번호 = (레이어 *2+1)^2 + 레이어 * 2 * 2
-						// 상방 기수번호 = (레이어 *2+1)^2 + 레이어 * 2 * 3
+                        // 우방 기수번호 = (레이어 *2+1)^2
+                        // 하방 기수번호 = (레이어 *2+1)^2 + 레이어 * 2 * 1
+                        // 좌방 기수번호 = (레이어 *2+1)^2 + 레이어 * 2 * 2
+                        // 상방 기수번호 = (레이어 *2+1)^2 + 레이어 * 2 * 3
 
-						// 우방 기수좌표 = -레이어 * TerrainPalette::nodeDistance, 0, +레이어 * TerrainPalette::nodeDistance
-						// 하방 기수번호 = +레이어 * TerrainPalette::nodeDistance, 0, +레이어 * TerrainPalette::nodeDistance
-						// 좌방 기수번호 = +레이어 * TerrainPalette::nodeDistance, 0, -레이어 * TerrainPalette::nodeDistance
-						// 상방 기수번호 = -레이어 * TerrainPalette::nodeDistance, 0, -레이어 * TerrainPalette::nodeDistance
+                        // 우방 기수좌표 = -레이어 * TerrainPalette::nodeDistance, 0, +레이어 * TerrainPalette::nodeDistance
+                        // 하방 기수번호 = +레이어 * TerrainPalette::nodeDistance, 0, +레이어 * TerrainPalette::nodeDistance
+                        // 좌방 기수번호 = +레이어 * TerrainPalette::nodeDistance, 0, -레이어 * TerrainPalette::nodeDistance
+                        // 상방 기수번호 = -레이어 * TerrainPalette::nodeDistance, 0, -레이어 * TerrainPalette::nodeDistance
 
-						// 우방~상방 = 0~3
-						double sx[4] = {
-							-layerIdx * TerrainPalette::nodeDistance,
-							+layerIdx * TerrainPalette::nodeDistance,
-							+layerIdx * TerrainPalette::nodeDistance,
-							-layerIdx * TerrainPalette::nodeDistance,
-						};
-						double sy[4] = {
-							+layerIdx * TerrainPalette::nodeDistance,
-							+layerIdx * TerrainPalette::nodeDistance,
-							-layerIdx * TerrainPalette::nodeDistance,
-							-layerIdx * TerrainPalette::nodeDistance,
-						};
-						constexpr double dx[4] = { TerrainPalette::nodeDistance, 0, -TerrainPalette::nodeDistance, 0 };
-						constexpr double dy[4] = { 0, -TerrainPalette::nodeDistance, 0, TerrainPalette::nodeDistance };
-						unsigned int lineType = layerIdx == 0 ? 0 : (i - (layerIdx * 2 - 1) * (layerIdx * 2 - 1)) / (layerIdx * 2);
-						double deltaIdx = layerIdx == 0 ? 0 : i - ((layerIdx * 2 - 1) * (layerIdx * 2 - 1) + layerIdx * 2 * lineType);
+                        // 우방~상방 = 0~3
+                        double sx[4] = {
+                            -layerIdx * TerrainPalette::nodeDistance,
+                            +layerIdx * TerrainPalette::nodeDistance,
+                            +layerIdx * TerrainPalette::nodeDistance,
+                            -layerIdx * TerrainPalette::nodeDistance,
+                        };
+                        double sy[4] = {
+                            +layerIdx * TerrainPalette::nodeDistance,
+                            +layerIdx * TerrainPalette::nodeDistance,
+                            -layerIdx * TerrainPalette::nodeDistance,
+                            -layerIdx * TerrainPalette::nodeDistance,
+                        };
+                        constexpr double dx[4] = { TerrainPalette::nodeDistance, 0, -TerrainPalette::nodeDistance, 0 };
+                        constexpr double dy[4] = { 0, -TerrainPalette::nodeDistance, 0, TerrainPalette::nodeDistance };
+                        unsigned int lineType = layerIdx == 0 ? 0 : (i - (layerIdx * 2 - 1) * (layerIdx * 2 - 1)) / (layerIdx * 2);
+                        double deltaIdx = layerIdx == 0 ? 0 : i - ((layerIdx * 2 - 1) * (layerIdx * 2 - 1) + layerIdx * 2 * lineType);
 
-						planeMesh->GetTransform()->SetLocalPosition( { sx[lineType] + deltaIdx * dx[lineType], 0, sy[lineType] + deltaIdx * dy[lineType] });
-						planeMesh->GetTransform()->SetLocalRotation( Vector3d{ 90,0,0 });
-						planeMeshesPos.push_back(planeMesh->GetTransform()->GetLocalPosition());
-					}
-					for (auto i = 0; i < planeMeshes.size(); i++)
-					{
-						if (i < expectedNum)
-							planeMeshes[i]->SetActive(true);
-						else
-							planeMeshes[i]->SetActive(false);
-					}
-				}
-			protected:
-				virtual void OnEnable() override
-				{
-					GetGameObject()->SetSelfActive(true);
-				}
-				virtual void OnDisable() override
-				{
-					GetGameObject()->SetSelfActive(false);
-				}
-			private:
-				/*
-				25 26 27 28 29 30 31
-				48 09 10 11 12 13 32
-				47 24 01 02 03 14 33
-				46 23 08 00 04 15 34
-				45 22 07 06 05 16 35
-				44 21 20 19 18 17 36
-				43 42 41 40 39 38 37
-				*/
-				vector<graphics::StaticMeshRenderer*> planeMeshes;
-				vector<Vector3d> planeMeshesPos;
-			};
-		}
-	}
+                        planeMesh->GetTransform()->SetLocalPosition({ sx[lineType] + deltaIdx * dx[lineType], 0, sy[lineType] + deltaIdx * dy[lineType] });
+                        planeMesh->GetTransform()->SetLocalRotation(Vector3d{ 90,0,0 });
+                        planeMeshesPos.push_back(planeMesh->GetTransform()->GetLocalPosition());
+                    }
+                    for (auto i = 0; i < planeMeshes.size(); i++)
+                    {
+                        if (i < expectedNum)
+                            planeMeshes[i]->SetActive(true);
+                        else
+                            planeMeshes[i]->SetActive(false);
+                    }
+                }
+            protected:
+                virtual void OnEnable() override
+                {
+                    SetBrushSize(brushSize);
+                }
+                virtual void OnDisable() override
+                {
+                }
+            private:
+                int brushSize{1};
+                vector<graphics::StaticMeshRenderer*> planeMeshes;
+                vector<Vector3d> planeMeshesPos;
+            };
+        }
+    }
 }
--- InWanderLand/TestCaseD3D11LiveObjImgui.cpp
+++ InWanderLand/TestCaseD3D11LiveObjImgui.cpp
@@ -1,47 +1,47 @@
-#include "InWanderLand.h"
-#ifdef GEN_TESTS
-#include "CppUnitTest.h"
-#include "ContentsLayer.h"
-#include "YunutyEngine.h"
-#include "DelayedTestFunctions.h"
-#include "Application.h"
-#include "RTSCam.h"
-#include "YunutyEngine.h"
-#include "DelayedTestFunctions.h"
-#include "ShakyCam.h"
-#include "DebugTilePlane.h"
-#include "DebugBeacon.h"
-#include "DebugMeshes.h"
-#include "YunutyEngine.h"
-#include <codecvt>
-
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
-// 이 함수는 게임의 기본 초기화 함수를 오버라이드합니다.
-void TestCaseD3D11LiveObjimguiInit()
-{
-}
-
-namespace tests
-{
-    TEST_CLASS(TestCaseD3D11LiveObjimguiClass)
-    {
-    public:
-        TEST_METHOD(TestCaseD3D11LiveObjimgui)
-        {
-            try
-            {
-                application::Application& client = application::Application::CreateApplication(0, 0);
-                application::contents::ContentsLayer::AssignTestInitializer(TestCaseD3D11LiveObjimguiInit);
-                client.Initialize();
-                client.Run();
-                client.Finalize();
-            }
-            catch (const std::exception& e)
-            {
-                Assert::Fail(yunutyEngine::yutility::GetWString(e.what()).c_str());
-            }
-        }
-    };
-}
-#endif
+//#include "InWanderLand.h"
+//#ifdef GEN_TESTS
+//#include "CppUnitTest.h"
+//#include "ContentsLayer.h"
+//#include "YunutyEngine.h"
+//#include "DelayedTestFunctions.h"
+//#include "Application.h"
+//#include "RTSCam.h"
+//#include "YunutyEngine.h"
+//#include "DelayedTestFunctions.h"
+//#include "ShakyCam.h"
+//#include "DebugTilePlane.h"
+//#include "DebugBeacon.h"
+//#include "DebugMeshes.h"
+//#include "YunutyEngine.h"
+//#include <codecvt>
+//
+//using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+//
+//// 이 함수는 게임의 기본 초기화 함수를 오버라이드합니다.
+//void TestCaseD3D11LiveObjimguiInit()
+//{
+//}
+//
+//namespace tests
+//{
+//    TEST_CLASS(TestCaseD3D11LiveObjimguiClass)
+//    {
+//    public:
+//        TEST_METHOD(TestCaseD3D11LiveObjimgui)
+//        {
+//            try
+//            {
+//                application::Application& client = application::Application::CreateApplication(0, 0);
+//                application::contents::ContentsLayer::AssignTestInitializer(TestCaseD3D11LiveObjimguiInit);
+//                client.Initialize();
+//                client.Run();
+//                client.Finalize();
+//            }
+//            catch (const std::exception& e)
+//            {
+//                Assert::Fail(yunutyEngine::yutility::GetWString(e.what()).c_str());
+//            }
+//        }
+//    };
+//}
+//#endif
