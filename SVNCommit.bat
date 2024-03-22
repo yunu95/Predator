231db1981b5e1846a9524906b86e2b0e1901e460
yunu95 <liunux95@gmail.com>
2024-03-22 19:49:38 +0900

    SVN 업데이트후 resolve하는 구문까지 삽입

--- SVNCommit.bat
+++ SVNCommit.bat
@@ -2,6 +2,7 @@
 
 svn cleanup C:\1_Executable\autoRelease
 svn update C:\1_Executable\autoRelease
+svn resolve C:\1_Executable\autoRelease/* --accept mine-full
 svn add C:\1_Executable\autoRelease/*
 svn commit C:\1_Executable\autoRelease -m "auto commit by jenkins"
 
