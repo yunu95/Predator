3aa9bbd36332bb6acab1ae677eadee69c9b11c84
qkrrbgus <gmfrwh12@gmail.com>
2024-05-14 17:32:44 +0900

    [PRED-16] Leader 유닛 방식이 추가됨에 따라 시네마틱 종료 로직 수정.

--- InWanderLand/GameManager.cpp
+++ InWanderLand/GameManager.cpp
@@ -105,13 +105,18 @@ void GameManager::EndCinematic()
 	/// 나머지 녀석들은 OffsetMove 로 변경
 	if (isBattleModeOn)
 	{
-		PlayerController::Instance().FindSelectedUnitByUnitType(Unit::UnitType::Magician)->SetUnitStateDirectly(Unit::UnitState::Idle);
-		PlayerController::Instance().FindSelectedUnitByUnitType(Unit::UnitType::Healer)->SetUnitStateDirectly(Unit::UnitState::Idle);
+		Unit* tempLeaderUnit = PlayerController::Instance().GetCurrentSelectedPlayerUnit();
+		
+		for (auto e : PlayerController::Instance().GetPlayerMap())
+		{
+			if (e.second == tempLeaderUnit)
+				continue;
+			e.second->SetUnitStateDirectly(Unit::UnitState::Idle);
+		}
 	}
 	else
 	{
-		PlayerController::Instance().FindSelectedUnitByUnitType(Unit::UnitType::Magician)->SetUnitStateDirectly(Unit::UnitState::OffsetMove);
-		PlayerController::Instance().FindSelectedUnitByUnitType(Unit::UnitType::Healer)->SetUnitStateDirectly(Unit::UnitState::OffsetMove);
+		PlayerController::Instance().ReportBattleEnded();
 	}
 }
 
