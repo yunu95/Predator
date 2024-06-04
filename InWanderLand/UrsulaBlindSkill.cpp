11e06c49cd8254cae6957016a2fcb46ecc3b7ab4
KSJ <junjunpp03@gmail.com>
2024-06-04 20:33:21 +0900

    [PRED-169] Ursula Q 작업 중

--- InWanderLand/UrsulaBlindSkill.cpp
+++ InWanderLand/UrsulaBlindSkill.cpp
@@ -1,19 +1,44 @@
 #include "InWanderLand.h"
 #include "UrsulaBlindSkill.h"
 
+Vector3d UrsulaBlindSkill::lastSkillPos = Vector3d();
+Vector3d UrsulaBlindSkill::lastSkillDir = Vector3d();
+
 coroutine::Coroutine UrsulaBlindSkill::operator()()
 {
+    lastSkillPos = targetPos;
+    lastSkillDir = (lastSkillPos - owner.lock()->GetGameObject()->GetTransform()->GetWorldPosition()).Normalized();
+    co_await std::suspend_always{};
+
+    
+
     co_return;
 }
 
 void UrsulaBlindSkill::OnInterruption()
 {
     projectile.lock()->SetRadius(0.5);
-    circle_1.lock()->SetRadius(0.5);
-    circle_2.lock()->SetRadius(0.5);
-    circle_3.lock()->SetRadius(0.5);
+    circle_Top.lock()->SetRadius(0.5);
+    circle_Left.lock()->SetRadius(0.5);
+    circle_Right.lock()->SetRadius(0.5);
     UnitAcquisitionSphereColliderPool::SingleInstance().Return(projectile);
-    UnitAcquisitionSphereColliderPool::SingleInstance().Return(circle_1);
-    UnitAcquisitionSphereColliderPool::SingleInstance().Return(circle_2);
-    UnitAcquisitionSphereColliderPool::SingleInstance().Return(circle_3);
+    UnitAcquisitionSphereColliderPool::SingleInstance().Return(circle_Top);
+    UnitAcquisitionSphereColliderPool::SingleInstance().Return(circle_Left);
+    UnitAcquisitionSphereColliderPool::SingleInstance().Return(circle_Right);
+}
+
+Vector3d UrsulaBlindSkill::GetSkillObjectPos_Top()
+{
+    auto finalPos = lastSkillPos;
+    return finalPos;
+}
+
+Vector3d UrsulaBlindSkill::GetSkillObjectPos_Left()
+{
+    return Vector3d();
+}
+
+Vector3d UrsulaBlindSkill::GetSkillObjectPos_Right()
+{
+    return Vector3d();
 }
--- InWanderLand/UrsulaBlindSkill.h
+++ InWanderLand/UrsulaBlindSkill.h
@@ -9,10 +9,17 @@ public:
     virtual SkillType::Enum GetSkillType() { return SkillType::URSULA_Q; }
     virtual coroutine::Coroutine operator()()override;
     virtual void OnInterruption()override;
+
+    /// 삼각형 대형으로 보았을 때, Top / Left / Right 위치값을 반환합니다.
+    static Vector3d GetSkillObjectPos_Top();
+    static Vector3d GetSkillObjectPos_Left();
+    static Vector3d GetSkillObjectPos_Right();
 private:
+    static Vector3d lastSkillPos;
+    static Vector3d lastSkillDir;
     std::weak_ptr<UnitAcquisitionSphereCollider> projectile;
-    std::weak_ptr<UnitAcquisitionSphereCollider> circle_1;
-    std::weak_ptr<UnitAcquisitionSphereCollider> circle_2;
-    std::weak_ptr<UnitAcquisitionSphereCollider> circle_3;
+    std::weak_ptr<UnitAcquisitionSphereCollider> circle_Top;
+    std::weak_ptr<UnitAcquisitionSphereCollider> circle_Left;
+    std::weak_ptr<UnitAcquisitionSphereCollider> circle_Right;
 };
 
