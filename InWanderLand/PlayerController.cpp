f034b8d712475029161ccc60071a368d332e1380
gyuhyeonPark <gmfrwh12@gmail.com>
2024-06-07 12:45:06 +0900

    [PRED-16] 테스트 용도로 헨젤의 스킬에 하트퀸 스킬을 넣었던 부분을 다시 복구.

--- InWanderLand/PlayerController.cpp
+++ InWanderLand/PlayerController.cpp
@@ -286,8 +286,8 @@ void PlayerController::ActivateSkill(SkillType::Enum skillType, Vector3d pos)
     case SkillType::ROBIN_W: selectedCharacter.lock()->OrderSkill(RobinTauntSkill{  }, pos); break;
     case SkillType::URSULA_Q: selectedCharacter.lock()->OrderSkill(UrsulaBlindSkill{  }, pos); break;
     case SkillType::URSULA_W: selectedCharacter.lock()->OrderSkill(UrsulaParalysisSkill{  }, pos); break;
-    case SkillType::HANSEL_Q: selectedCharacter.lock()->OrderSkill(BossSpinAttackSkill{}, pos); break;
-    case SkillType::HANSEL_W: selectedCharacter.lock()->OrderSkill(BossImpaleSkill{}, pos); break;
+    case SkillType::HANSEL_Q: selectedCharacter.lock()->OrderSkill(HanselChargeSkill{}, pos); break;
+    case SkillType::HANSEL_W: selectedCharacter.lock()->OrderSkill(HanselProjectileSkill{}, pos); break;
     }
     // 스킬 프리뷰를 비활성화시킨다.
 }
