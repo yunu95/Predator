ad133e48efe8ce9014d4fc71229ada411498a9a9
yunu95 <liunux95@gmail.com>
2024-06-11 15:05:38 +0900

    패시브 스킬들 추가, 코루틴 모듈화 구조 정의

---
+++ InWanderLand/PassiveUrsulaSpeed.cpp
@@ -0,0 +1,15 @@
+#include "PassiveUrsulaSpeed.h"
+#include "InWanderLand.h"
+
+POD_PassiveUrsulaSpeed PassiveUrsula::pod;
+void PassiveUrsula::Init(std::weak_ptr<Unit> owner)
+{
+    owner.lock()->onAttack.AddCallback([this, owner]()
+        {
+            owner.lock()->ApplyBuff(UnitBuffUrsula{});
+        });
+    owner.lock()->onDamaged.AddCallback([this, owner]()
+        {
+            owner.lock()->EraseBuff(UnitBuffType::UrsulaBuff);
+        });
+}
