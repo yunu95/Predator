c068c1ad128e4599990e459a8c1a37511b7bef80
gyuhyeonPark <gmfrwh12@gmail.com>
2024-06-24 20:08:54 +0900

    [PRED-16] BarricadeBox를 에디터 상에서 설치하면 투사체를 막는 기능을 한다.

--- InWanderLand/Interactable_BarricadeBox.h
+++ InWanderLand/Interactable_BarricadeBox.h
@@ -5,7 +5,17 @@
 class Interactable_BarricadeBox
 	: public IInteractableComponent
 {
+	Vector3d initPos = Vector3d(0, 0, 0);
+	Quaternion initRotation = Quaternion();
+	Vector3d initScale = Vector3d(1, 1, 1);
+
+
+
+	virtual void SetDataFromEditorData(const application::editor::InteractableData& data) override;
+
+	virtual void Start() override;
 	virtual void Update() {}
+	virtual void OnTriggerEnter(physics::Collider* collider) override;
 	virtual yunutyEngine::coroutine::Coroutine DoInteraction() override { co_return; }
 };
 
