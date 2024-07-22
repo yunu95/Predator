a0fac30dac00135ec1455acdd131a2f58ad1bd25
KSC <oeodo0116@naver.com>
2024-07-22 18:56:51 +0900

    [PRED-267] Animator에서 Play함수가 불렸을 때 nextAnimation이 있다면 초기화 하는 코드 추가 커밋

--- Yunuty/Animator.cpp
+++ Yunuty/Animator.cpp
@@ -4,148 +4,156 @@
 using namespace yunutyEngine::graphics;
 
 yunutyEngine::graphics::Animator::Animator()
-    : Renderable<yunuGI::IAnimator>(_YunuGIObjects::SingleInstance().factory->CreateAnimator({}))
+	: Renderable<yunuGI::IAnimator>(_YunuGIObjects::SingleInstance().factory->CreateAnimator({}))
 {
 
 }
 
 void Animator::Pause()
 {
-    isPlay = false;
+	isPlay = false;
 }
 
 void yunutyEngine::graphics::Animator::Resume()
 {
-    isPlay = true;
+	isPlay = true;
 }
 
 void Animator::ClearAnimationEvent(yunuGI::IAnimation* animation)
 {
-    for (auto& [key, each] : this->animationEventMap[animation])
-    {
-        if (each.isFirst && each.must)
-        {
-            each.func();
-        }
-        each.isFirst = true;
-    }
+	for (auto& [key, each] : this->animationEventMap[animation])
+	{
+		if (each.isFirst && each.must)
+		{
+			each.func();
+		}
+		each.isFirst = true;
+	}
 }
 
 void Animator::ChangeAnimation(yunuGI::IAnimation* animation, float transitionDuration, float transitionSpeed)
 {
-    auto& gi = this->GetGI();
+	auto& gi = this->GetGI();
 
-    gi.ChangeAnimation(animation, transitionDuration, transitionSpeed);
-    isChange = true;
+	gi.ChangeAnimation(animation, transitionDuration, transitionSpeed);
+	isChange = true;
 }
 yunuGI::IAnimation* yunutyEngine::graphics::Animator::GetCurrentAnimation()
 {
-    return GetGI().GetCurrentAnimation();
+	return GetGI().GetCurrentAnimation();
 }
 bool Animator::IsDone()
 {
-    if (auto currentAnim = GetGI().GetCurrentAnimation(); currentAnim)
-    {
-        // 애니메이션 보간중이 아닐 때
-        if (GetGI().GetTransitionDesc().next.animIndex == -1)
-        {
-            return GetGI().GetTransitionDesc().curr.sumTime >= currentAnim->GetDuration();
-        }
-        else
-        {
-            return false;
-        }
-    }
-    return true;
+	if (auto currentAnim = GetGI().GetCurrentAnimation(); currentAnim)
+	{
+		// 애니메이션 보간중이 아닐 때
+		if (GetGI().GetTransitionDesc().next.animIndex == -1)
+		{
+			return GetGI().GetTransitionDesc().curr.sumTime >= currentAnim->GetDuration();
+		}
+		else
+		{
+			return false;
+		}
+	}
+	return true;
 }
 
 void Animator::SetAnimationFrame(yunuGI::IAnimation* animation, unsigned int frame)
 {
-    isPlay = false;
+	isPlay = false;
 
-    auto& gi = this->GetGI();
+	auto& gi = this->GetGI();
 
-    gi.SetAnimationFrame(animation, frame);
+	gi.SetAnimationFrame(animation, frame);
 }
 
 void Animator::Play(yunuGI::IAnimation* animation)
 {
-    this->isPlay = true;
+	this->isPlay = true;
 
-    auto& gi = this->GetGI();
+	auto& gi = this->GetGI();
 
-    gi.Play(animation);
+	gi.Play(animation);
+
+	auto nextAnimation = gi.GetNextAnimation();
+	if (nextAnimation)
+	{
+		ClearAnimationEvent(nextAnimation);
+		gi.GetTransitionDesc().ClearNextAnimation();
+		gi.SetNextAnimation(nullptr);
+	}
 }
 
 void Animator::PushAnimation(yunuGI::IAnimation* animation)
 {
-    auto& gi = this->GetGI();
+	auto& gi = this->GetGI();
 
-    gi.PushAnimation(animation);
+	gi.PushAnimation(animation);
 }
 
 unsigned long long Animator::PushAnimationWithFunc(yunuGI::IAnimation* animation, unsigned int frame, std::function<void()> func, bool must)
 {
-    auto& gi = this->GetGI();
+	auto& gi = this->GetGI();
 
-    gi.PushAnimation(animation);
+	gi.PushAnimation(animation);
 
-    AnimationEvent animationEvent;
-    animationEvent.frame = frame;
-    animationEvent.func = func;
-    animationEvent.isFirst = true;
-    animationEvent.must = must;
+	AnimationEvent animationEvent;
+	animationEvent.frame = frame;
+	animationEvent.func = func;
+	animationEvent.isFirst = true;
+	animationEvent.must = must;
 
-    this->animationEventMap[animation][functorIndex] = animationEvent;
-    return functorIndex++;
+	this->animationEventMap[animation][functorIndex] = animationEvent;
+	return functorIndex++;
 }
 
 bool Animator::EraseAnimationFunc(yunuGI::IAnimation* animation, unsigned long long index)
 {
-    if (animationEventMap.contains(animation) && animationEventMap[animation].contains(index))
-    {
-        animationEventMap[animation].erase(index);
-        return true;
-    }
+	if (animationEventMap.contains(animation) && animationEventMap[animation].contains(index))
+	{
+		animationEventMap[animation].erase(index);
+		return true;
+	}
 
-    return false;
+	return false;
 }
 
 float yunutyEngine::graphics::Animator::GetCurrentFrame()
 {
-    auto& gi = this->GetGI();
-    auto& desc = gi.GetTransitionDesc();
+	auto& gi = this->GetGI();
+	auto& desc = gi.GetTransitionDesc();
 
-    return desc.curr.currFrame;
+	return desc.curr.currFrame;
 }
 
 void Animator::OnEnable()
 {
-    if (this->GetGI().GetCurrentAnimation())
-    {
-        this->Play(this->GetGI().GetCurrentAnimation());
-    }
+	if (this->GetGI().GetCurrentAnimation())
+	{
+		this->Play(this->GetGI().GetCurrentAnimation());
+	}
 }
 
 void Animator::OnDisable()
 {
-
+	///ClearAnimationEvent(this->GetGI().GetCurrentAnimation());
 }
 
 void Animator::Update()
 {
-    auto& gi = this->GetGI();
-    auto& desc = gi.GetTransitionDesc();
+	auto& gi = this->GetGI();
+	auto& desc = gi.GetTransitionDesc();
 
-    if (gi.GetCurrentAnimation() == nullptr)
-    {
-        return;
-    }
+	if (gi.GetCurrentAnimation() == nullptr)
+	{
+		return;
+	}
 
-    if (isPlay)
-    {
-        // 현재 애니메이션 업데이트
-        {
+	if (isPlay)
+	{
+		// 현재 애니메이션 업데이트
+		{
 			auto currentAnimation = gi.GetCurrentAnimation();
 			float duration = currentAnimation->GetDuration();
 			int totalFrame = currentAnimation->GetTotalFrame();
@@ -165,35 +173,35 @@ void Animator::Update()
 			desc.curr.currFrame = min(static_cast<int>(desc.curr.currFrame), totalFrame - 1);
 			desc.curr.nextFrame = min(static_cast<int>(desc.curr.currFrame + 1), totalFrame - 1);
 			desc.curr.ratio = (desc.curr.sumTime - (desc.curr.currFrame * frameDuration)) / frameDuration;
-        }
+		}
 
-        for (auto& each : this->animationEventMap)
-        {
-            auto curAnimation = gi.GetCurrentAnimation();
-            for (auto& [key, each2] : each.second)
-            {
-                if (curAnimation->GetName() == each.first->GetName())
-                {
-                    if (each2.frame == desc.curr.currFrame)
-                    {
-                        if (each2.isFirst && (each2.func != nullptr))
-                        {
-                            each2.isFirst = false;
-                            each2.func();
-                        }
-                    }
-                    else
-                    {
-                        each2.isFirst = true;
-                    }
-                }
-            }
-        }
+		for (auto& each : this->animationEventMap)
+		{
+			auto curAnimation = gi.GetCurrentAnimation();
+			for (auto& [key, each2] : each.second)
+			{
+				if (curAnimation->GetName() == each.first->GetName())
+				{
+					if (each2.frame == desc.curr.currFrame)
+					{
+						if (each2.isFirst && (each2.func != nullptr))
+						{
+							each2.isFirst = false;
+							each2.func();
+						}
+					}
+					else
+					{
+						each2.isFirst = true;
+					}
+				}
+			}
+		}
 
-        auto nextAnimation = gi.GetNextAnimation();
-        // 다음 애니메이션이 있다면
-        if (nextAnimation)
-        {
+		auto nextAnimation = gi.GetNextAnimation();
+		// 다음 애니메이션이 있다면
+		if (nextAnimation)
+		{
 			float duration = nextAnimation->GetDuration();
 			int totalFrame = nextAnimation->GetTotalFrame();
 			totalFrame = totalFrame + 1;
@@ -227,32 +235,32 @@ void Animator::Update()
 				desc.next.nextFrame = min(static_cast<int>(desc.next.currFrame + 1), totalFrame - 1);
 				desc.next.ratio = (desc.next.sumTime - (desc.next.currFrame * frameDuration)) / frameDuration;
 
-                // 블렌딩 중인 애니메이션에 대한 이벤트까지 처리
-                for (auto& each : this->animationEventMap)
-                {
-                    auto nextAnimation = gi.GetNextAnimation();
-                    for (auto& [key, each2] : each.second)
-                    {
-                        if (nextAnimation->GetName() == each.first->GetName())
-                        {
-                            if (each2.frame == desc.next.currFrame)
-                            {
-                                if (each2.isFirst && (each2.func != nullptr))
-                                {
-                                    each2.isFirst = false;
-                                    each2.func();
-                                }
-                            }
-                            else
-                            {
-                                each2.isFirst = true;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+				// 블렌딩 중인 애니메이션에 대한 이벤트까지 처리
+				for (auto& each : this->animationEventMap)
+				{
+					auto nextAnimation = gi.GetNextAnimation();
+					for (auto& [key, each2] : each.second)
+					{
+						if (nextAnimation->GetName() == each.first->GetName())
+						{
+							if (each2.frame == desc.next.currFrame)
+							{
+								if (each2.isFirst && (each2.func != nullptr))
+								{
+									each2.isFirst = false;
+									each2.func();
+								}
+							}
+							else
+							{
+								each2.isFirst = true;
+							}
+						}
+					}
+				}
+			}
+		}
 
-        gi.SetTransitionDesc(desc);
-    }
+		gi.SetTransitionDesc(desc);
+	}
 }
